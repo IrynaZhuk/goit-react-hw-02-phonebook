ac3f505a0a91c14e461182956eb5d0fb6b7cc857
IrynaZhuk <irynazh94@gmail.com>
2022-08-12 13:17:53 +0300

    phone-book

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
-import { AddContactForm } from './AddContactForm/AddContactForm';
+import { nanoid } from 'nanoid';
+import { Contacts } from 'components/Contacts/Contacts';
+import { AddContactForm } from 'components/AddContactForm/AddContactForm';
+import { Filter } from 'components/Filter/Filter';
+import styles from './App.module.css';
 
 export class App extends Component {
   state = {
     contacts: [],
-    name: '',
+    filter: '',
+  };
+
+  deleteContact = e => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(
+        contact => contact.id !== e.target.id
+      ),
+    }));
+  };
+
+  filterChange = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  contactsChange = (name, number) => {
+    this.setState(prevState => {
+      if (
+        prevState.contacts.find(contact =>
+          contact.name.toLowerCase().includes(name.toLowerCase())
+        )
+      ) {
+        return alert(`${name} is already in contacts`);
+      }
+      return {
+        contacts: [
+          ...prevState.contacts,
+          { name: name, number: number, id: nanoid() },
+        ],
+      };
+    });
   };
 
   render() {
     return (
       <>
-        <section>
-          <h1>Phonebook</h1>
-          <AddContactForm></AddContactForm>
+        <section className={styles.section}>
+          <h1 className={styles.header}>Phonebook</h1>
+
+          <AddContactForm contactsChange={this.contactsChange} />
+          <h2 className={styles.header}>Contacts</h2>
+          <Filter filterChange={this.filterChange} />
+          <Contacts
+            contacts={this.state.contacts}
+            filter={this.state.filter}
+            deleteContact={this.deleteContact}
+          />
         </section>
       </>
     );

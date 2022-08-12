1f64acd6c0c3177eaea94557ee2e6787ca3abd9e
IrynaZhuk <irynazh94@gmail.com>
2022-08-12 10:27:35 +0300

    json/homepage

---
+++ src/components/AddContactForm/AddContactForm.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export class AddContactForm extends Component {
+  state = {
+    name: '',
+  };
+
+  handleChange = () => {};
+
+  render() {
+    return (
+      <form>
+        Name
+        <label htmlFor="">
+          <input
+            onChange={this.handleChange}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
+}
